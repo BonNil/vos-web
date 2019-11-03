4688c4eb420ad4b5ce0638654808c88d9e81129d
BonNil <boonnyn@gmail.com>
2019-11-03 22:07:58 +0100

    Additional content changes

--- src/components/Employees/index.js
+++ src/components/Employees/index.js
@@ -26,7 +26,7 @@ export default function Employees(props) {
 					</div>
 					<div className='info'>
 						<label>{employee.name}</label>
-						<div className='started'>Anställd {employee.started}</div>
+						<div className='started'>{employee.started}</div>
 						<div className='role'>{employee.role}</div>
 					</div>
 				</div>
