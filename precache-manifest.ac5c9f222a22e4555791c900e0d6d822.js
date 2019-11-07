06c08612ed66bf4720b72257e78af4faa8470f72
Bonny Nilsson <bonny.nilsson@decerno.se>
2019-11-07 10:19:27 +0100

    Updates

---
+++ precache-manifest.ac5c9f222a22e4555791c900e0d6d822.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de5285b7dd7658df5a1df24b94cf3c2",
+    "url": "/vos-web/index.html"
+  },
+  {
+    "revision": "3d2152532a862a13527d",
+    "url": "/vos-web/static/css/2.910f75eb.chunk.css"
+  },
+  {
+    "revision": "7747f40173d15372d241",
+    "url": "/vos-web/static/css/main.7525de3e.chunk.css"
+  },
+  {
+    "revision": "3d2152532a862a13527d",
+    "url": "/vos-web/static/js/2.f4986e80.chunk.js"
+  },
+  {
+    "revision": "7747f40173d15372d241",
+    "url": "/vos-web/static/js/main.5206d6b1.chunk.js"
+  },
+  {
+    "revision": "f78b8148814774de7292",
+    "url": "/vos-web/static/js/runtime~main.f8236d73.js"
+  },
+  {
+    "revision": "244145cc195bff962c0bb160a9bbf7ae",
+    "url": "/vos-web/static/media/CASE_CX245D-SR.244145cc.jpg"
+  },
+  {
+    "revision": "30af21a6d726a6111b8ca900de9dd5d2",
+    "url": "/vos-web/static/media/CASE_CX250D.30af21a6.jpg"
+  },
+  {
+    "revision": "694ae86992be794b47d6fd7c2f149ffa",
+    "url": "/vos-web/static/media/CASE_CX290B.694ae869.jpg"
+  },
+  {
+    "revision": "3d1ca4fca2468d8f2e218c266d20141f",
+    "url": "/vos-web/static/media/CASE_CX300D.3d1ca4fc.jpg"
+  },
+  {
+    "revision": "1cb5cac40a900c6245209951dd794698",
+    "url": "/vos-web/static/media/CASE_CX75C-SR.1cb5cac4.jpg"
+  },
+  {
+    "revision": "1545736ed3e9746d0a3e390c1973c4a3",
+    "url": "/vos-web/static/media/CAT_910K.1545736e.jpg"
+  },
+  {
+    "revision": "a89a460250e50506ebeb682a50a11167",
+    "url": "/vos-web/static/media/CAT_D6T_LGP-V-PAT_steg3A.a89a4602.jpg"
+  },
+  {
+    "revision": "c92d2947f511ead04a15251b30d095f0",
+    "url": "/vos-web/static/media/CAT_D6T_LGP-V-PAT_steg4.c92d2947.jpg"
+  },
+  {
+    "revision": "7d149debd7a798667c3421c56adad1d5",
+    "url": "/vos-web/static/media/Dan_Nilsson.7d149deb.jpg"
+  },
+  {
+    "revision": "e625023beecb2d4ff2b388e43ef86e7c",
+    "url": "/vos-web/static/media/IMG_7552.e625023b.jpg"
+  },
+  {
+    "revision": "fdf33043e8f1718474889babbe4283f7",
+    "url": "/vos-web/static/media/IMG_7591.fdf33043.jpg"
+  },
+  {
+    "revision": "4b1d2d009f375a6658514cb826f0923e",
+    "url": "/vos-web/static/media/IMG_7592.4b1d2d00.jpg"
+  },
+  {
+    "revision": "496315287538751478cc15606efd02ac",
+    "url": "/vos-web/static/media/IMG_7593.49631528.jpg"
+  },
+  {
+    "revision": "42cd89943b4f933e1295a1950493a885",
+    "url": "/vos-web/static/media/IMG_7594.42cd8994.jpg"
+  },
+  {
+    "revision": "a457d64eae0e88e6ab0ce1bc85bb6e32",
+    "url": "/vos-web/static/media/IMG_7595.a457d64e.jpg"
+  },
+  {
+    "revision": "8d9d7eb498bfc5b8c87b60218f771732",
+    "url": "/vos-web/static/media/IMG_7596.8d9d7eb4.jpg"
+  },
+  {
+    "revision": "94a7e32f6faed5e27ca5cce51423a987",
+    "url": "/vos-web/static/media/IMG_7597.94a7e32f.jpg"
+  },
+  {
+    "revision": "5ed956acc17fa9e1c20b96c78fe9536d",
+    "url": "/vos-web/static/media/IMG_7598.5ed956ac.jpg"
+  },
+  {
+    "revision": "2a6a02a9af84e7448d3e5708bcd4edfa",
+    "url": "/vos-web/static/media/IMG_7599.2a6a02a9.jpg"
+  },
+  {
+    "revision": "79b44ffc992f85fe3a1b15d0de1d2a29",
+    "url": "/vos-web/static/media/Johan_Nilsson.79b44ffc.jpg"
+  },
+  {
+    "revision": "d3f240674768132ade6ce30adfd54873",
+    "url": "/vos-web/static/media/Noah_Edensten.d3f24067.jpg"
+  },
+  {
+    "revision": "656eed0498f0f3fc27cda5faac962523",
+    "url": "/vos-web/static/media/VOLVO_EWR150E.656eed04.jpg"
+  },
+  {
+    "revision": "86f81815aa031dd6d319e747ce69aabf",
+    "url": "/vos-web/static/media/VOLVO_FM11.86f81815.jpg"
+  },
+  {
+    "revision": "ddad63e026cc1a04c5bdb50a79ebac32",
+    "url": "/vos-web/static/media/bjorn.ddad63e0.jpg"
+  },
+  {
+    "revision": "1df73057af7eb6e572392e46ed690a15",
+    "url": "/vos-web/static/media/elias.1df73057.jpg"
+  },
+  {
+    "revision": "793c4568466a86318d04bd538d0e75c2",
+    "url": "/vos-web/static/media/fredrik.793c4568.jpg"
+  },
+  {
+    "revision": "954301733134abee044d3f14262dd901",
+    "url": "/vos-web/static/media/hero_1920.95430173.jpg"
+  },
+  {
+    "revision": "7d32baabb5fb6d9bb4c4192992965e72",
+    "url": "/vos-web/static/media/jorgen.7d32baab.jpg"
+  },
+  {
+    "revision": "81fe148f52b9da025f9742c0f87dee58",
+    "url": "/vos-web/static/media/lill.81fe148f.jpg"
+  },
+  {
+    "revision": "c56d2370e1ef9188e13bd20b5a56228b",
+    "url": "/vos-web/static/media/original_logo.c56d2370.png"
+  },
+  {
+    "revision": "b96e75af7ec0bdde09e30bcd54074d6c",
+    "url": "/vos-web/static/media/robban.b96e75af.jpg"
+  }
+]);
