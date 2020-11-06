d522d9f7ed9578a032cdde7b4756215a168305d2
BonNil <boonnyn@gmail.com>
2020-11-06 21:55:43 +0100

    simplified image gallery, no modal

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,40 +1,16 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
 import Gallery from 'react-photo-gallery';
-import Carousel, { Modal, ModalGateway } from 'react-images';
 import photos from './photos';
 import './gallery.sass';
 
 export default function ImageGallery() {
-	const [currentImage, setCurrentImage] = useState(0);
-	const [viewerIsOpen, setViewerIsOpen] = useState(false);
-
-	const openLightbox = useCallback((event, { photo, index }) => {
-		setCurrentImage(index);
-		setViewerIsOpen(true);
-	}, []);
-
-	const closeLightbox = () => {
-		setCurrentImage(0);
-		setViewerIsOpen(false);
-	};
 
 	return (
 		<div className='imageGallery'>
-			<Gallery photos={photos} onClick={openLightbox} direction={'column'} />
-			<ModalGateway>
-				{viewerIsOpen ? (
-					<Modal onClose={closeLightbox} allowFullscreen={false}>
-						<Carousel
-							currentIndex={currentImage}
-							views={photos.map(x => ({
-								...x,
-								srcset: x.srcSet,
-								caption: x.title,
-							}))}
-						/>
-					</Modal>
-				) : null}
-			</ModalGateway>
+			<Gallery
+				photos={photos}
+				onClick={(event, { photo, index }) => window.open(photo.src, "_blank")}
+				direction={'column'} />
 		</div>
 	);
 }
