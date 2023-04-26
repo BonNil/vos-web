4daad45b4b7104f434fff7c53f261efc70c1001d
Bonny Nilsson <bonny@montel.no>
2023-04-26 12:33:56 +0200

    Added new employee, removed old ones

--- src/components/Employees/employeeImages.js
+++ src/components/Employees/employeeImages.js
@@ -8,10 +8,10 @@ import elias from '../../assets/images/employees/elias_350.webp';
 import johanEspenkrona from '../../assets/images/employees/johan_e_350.webp';
 import edvin from '../../assets/images/employees/edvin_350.webp';
 import anton from '../../assets/images/employees/anton_350.webp';
-import tobias from '../../assets/images/employees/tobias_350.webp';
 import alexander from '../../assets/images/employees/alexander_350.webp';
 import niklas from '../../assets/images/employees/niklas_m_350.webp';
 import adam from '../../assets/images/employees/adam_n_350.webp';
+import sebastian from '../../assets/images/employees/sebastian_350.webp';
 
 export default [
 	{ name: 'Robert Nilsson', url: robban, alt: 'Robbert Nilsson, Grundare, VD och ägare' },
@@ -24,8 +24,8 @@ export default [
 	{ name: 'Johan Espenkrona', url: johanEspenkrona, alt: 'Johan Espenkrona, Anläggare och lastbilschaufför' },
 	{ name: 'Anton Trulsson', url: anton, alt: 'Anton Trulsson, Hjulgrävarmaskinist' },
 	{ name: 'Edvin Andersson', url: edvin, alt: 'Edvin Andersson, Grävmaskinist' },
-	{ name: 'Tobias Wikström', url: tobias, alt: 'Tobias Wikström, Grävmaskinist' },
 	{ name: 'Aleczander Holm', url: alexander, alt: 'Aleczander Holm, Anläggare' },
 	{ name: 'Niklas Möller', url: niklas, alt: 'Niklas Möller, Anläggare' },
 	{ name: 'Adam Nilsson', url: adam, alt: 'Adam Nilsson, Grävmaskinist' },
+	{ name: 'Sebastian Hjort', url: sebastian, alt: 'Sebastian Hjort, Grävmaskinist/Bandtraktorpilot' },
 ];
